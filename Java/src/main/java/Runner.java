5d5a78c0d843538b02afd7ef24fd00732cdb0b1e
Кристина Миневич <kristina.minevich73@gmail.com>
2022-11-03 20:20:30 +0300

    Tests refactor

--- Java/src/main/java/Runner.java
+++ Java/src/main/java/Runner.java
@@ -1,5 +1,5 @@
 import models.MilitaryType;
-import Planes.MilitaryPlane;
+import Planes.militaryPlane;
 import Planes.PassengerPlane;
 import Planes.Plane;
 
@@ -16,12 +16,12 @@ public class Runner {
             new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
             new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
             new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
-            new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
-            new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
-            new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
-            new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
-            new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
-            new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
+            new militaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
+            new militaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+            new militaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+            new militaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+            new militaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+            new militaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
     );
 
     public static void main(String[] args) {
